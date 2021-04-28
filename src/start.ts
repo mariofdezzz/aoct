035e775c7bf8c69fe8a0858b89436cf097d927e0
mariofdezzz <marioferrerofdez@gmail.com>
2021-04-28 20:14:21 +0100

    ♻ Make start async

--- src/start.ts
+++ src/start.ts
@@ -1,22 +1,41 @@
-import { mkdirSync, existsSync, copyFileSync } from 'fs'
+import { constants } from 'fs'
+import { mkdir, access, copyFile } from 'fs/promises'
 import { resolve } from 'path'
 import { spawn } from 'child_process'
 import getConfig from './config'
+const ora = require('ora')
 const { compiler, year } = getConfig()
+const { F_OK } = constants
 
-export default (day: string) => {
+export default async (day: string) => {
   process.env.DAY = day
 
-  mkdirSync(`./src/${year}`, { recursive: true })
+  const spinner = ora()
+  spinner.start('Preparing files')
 
-  if (!existsSync(`./src/${year}/${day}.${compiler}`))
-    copyFileSync(
-      resolve(__dirname, `../templates/index.${compiler}`),
-      `./src/${year}/${day}.${compiler}`
+  try {
+    await mkdir(`./src/${year}`, { recursive: true })
+
+    try {
+      await access(`./src/${year}/${day}.${compiler}`, F_OK)
+    } catch (error) {
+      await copyFile(
+        resolve(__dirname, `../templates/index.${compiler}`),
+        `./src/${year}/${day}.${compiler}`
+      )
+    }
+    spinner.succeed()
+
+    spawn(
+      'npx',
+      ['nodemon', '--quiet', `./src/${year}/${process.env.DAY}.${compiler}`],
+      {
+        shell: true,
+        stdio: 'inherit'
+      }
     )
-
-  spawn('npx', ['nodemon', '--quiet', `./src/${year}/${day}.${compiler}`], {
-    shell: true,
-    stdio: 'inherit'
-  })
+  } catch (error) {
+    spinner.fail()
+    console.error(error)
+  }
 }
