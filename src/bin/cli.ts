94d4045a431df090eb1f20f30a507bf346fc6311
mariofdezzz <marioferrerofdez@gmail.com>
2021-08-06 22:56:18 +0100

    🎨 Move cli.js to bin folder

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import yargs = require('yargs/yargs')
-import start from './components/start'
+import start from '../components/start'
 
 const argv = yargs(process.argv.slice(2))
   .command('start <day>', 'Executes given day', yargs => {
