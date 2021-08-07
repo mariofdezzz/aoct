ba19798fcd0d7ddad138b8aae812f7ad22f24243
mariofdezzz <marioferrerofdez@gmail.com>
2021-08-07 13:23:07 +0100

    🎨 Linter fix read.ts

--- src/assets/data/read.ts
+++ src/assets/data/read.ts
@@ -3,9 +3,12 @@ import { access, readFile } from 'fs/promises'
 
 const { F_OK } = constants
 
-export default async (file: string): Promise<Array<string>> => {
-  let config = JSON.parse(process.env.CONFIG)
-  const path = `./data/${config.year}/${process.env.DAY}/${file}`
+export default async (file: string): Promise<string[]> => {
+  const { year }: {year: string} = JSON.parse(
+    process.env.CONFIG ?? '{"year":2015}'
+  )
+  const day = process.env.DAY ?? 'day1'
+  const path = `./data/${year}/${day}/${file}`
 
   try {
     await access(path, F_OK)
