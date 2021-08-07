ddb09905446bebac8334e9b8258d38302e1d8907
mariofdezzz <marioferrerofdez@gmail.com>
2021-08-07 13:01:00 +0100

    🎨 Linter fix log.ts

--- src/assets/performance/log.ts
+++ src/assets/performance/log.ts
@@ -1,3 +1,8 @@
-export default (title, { solution, time }) => {
+interface logInput {
+  solution: number
+  time: number
+}
+
+export default (title: string, { solution, time }: logInput): void => {
   console.log('\x1b[0m\x1b[32m' + `[${time}ms]` + '\x1b[0m', title, solution)
 }
